9e4d788484111b6a30c68b40145da4adcf6d12bd
Cailie Skelton <cskelton707@gmail.com>
2020-04-19 18:05:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 def countdown(num)
-  while num < 0
+  while num > 0
   puts "#{num} SECONDS!"
-  num += 1
+  num -= 1
 end
 puts "HAPPY NEW YEAR!"
 end
@@ -11,7 +11,7 @@ end
 def countdown_with_sleep(seconds)
   while num > 1
   puts "#{num} SECOND(S)!"
-  sleep(1)
+sleep(1)
   num -= 1
 end
 end
