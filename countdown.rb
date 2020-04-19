84c911465cdf07c1bc9f697cb92485bd0db620e8
Cailie Skelton <cskelton707@gmail.com>
2020-04-19 17:01:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,12 @@
 #write your code here
-
 def countdown(10)
-  x = 1 
+  x = 1
   while x < 10
   puts "#{x} SECONDS!"
   x += 1
  end
+ puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(10.seconds)
 end
