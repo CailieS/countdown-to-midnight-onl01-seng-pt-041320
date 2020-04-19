ae8cf83b40471e20806982a164d1b03a6557637c
Cailie Skelton <cskelton707@gmail.com>
2020-04-19 17:04:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(num)
  puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(10.seconds)
-  
+
+def countdown_with_sleep(seconds)
+  while num > 1
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+end
 end
